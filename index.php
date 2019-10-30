8649ddeddb481057f76cfa1c59858b0fdd0014c0
Maltheboss <maltheminecraft@gmail.com>
2019-10-30 13:35:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-
 <?php
-//including the database connection file
-include_once("config.php");
 
-//fetching data in descending order (lastest entry first)
-$result = $dbConn->query("SELECT * FROM users ORDER BY id DESC");
+include "alternate/db.php";
+
+$result = $dbConn->query('SELECT * FROM users ORDER BY id DESC');
 ?>
 
 <html>
@@ -13,25 +11,26 @@ $result = $dbConn->query("SELECT * FROM users ORDER BY id DESC");
 </head>
 
 <body>
-<a href="add.html">Add New Data</a><br/><br/>
+<a class="create" href="create.php">Create new user</a><br/><br/>
 
 	<table width='80%' border=0>
 
 	<tr bgcolor='#CCCCCC'>
-		<td>Name</td>
-		<td>Age</td>
-		<td>Email</td>
-		<td>Update</td>
+		<th>Username</th>
+		<th>Email</th>
+		<th>Password</th>
+		<th>Update</th>
 	</tr>
 	<?php 	
 	while($row = $result->fetch(PDO::FETCH_ASSOC)) { 		
-		echo "<tr>";
-		echo "<td>".$row['name']."</td>";
-		echo "<td>".$row['age']."</td>";
-		echo "<td>".$row['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$row[id]\">Edit</a> | <a href=\"delete.php?id=$row[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+		echo '<tr>';
+		echo '<td>'.$row['username'].'</td>';
+		echo '<td>'.$row['email'].'</td>';
+		echo '<td>'.$row['password'].'</td>';	
+		echo "<td><a class='update' href=\"update.php?id=$row[id]\">Edit</a> | <a class='delete' href=\"delete.php?id=$row[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
 	}
 	?>
 	</table>
 </body>
 </html>
+
