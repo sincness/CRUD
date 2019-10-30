5589c0dccb754a2b9b90f7c1df1ad44ada85f249
kriztiank <kriztiank@gmail.com>
2019-10-30 10:14:13 +0100

    db created

--- alternate/db.php
+++ alternate/db.php
@@ -1,1 +1,19 @@
 
+<!-- contains database connection code.
+This file is included in all PHP pages where database connection is necessary. -->
+
+<?php
+$host = 'localhost';
+$dbname = 'crud';
+$user = 'root';
+$pass = 'root';
+
+try {
+    // http://php.net/manual/en/pdo.connections.php
+    $dbConn = new PDO("mysql:host={$host};dbname={$dbname}", $user, $pass);
+
+    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Setting Error Mode as Exception
+    // More on setAttribute: http://php.net/manual/en/pdo.setattribute.php
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
