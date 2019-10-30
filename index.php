eaa6068d7329c250206467ba2cdf8211deb1cc44
Daniel Jensen <45940133+Darkrenga@users.noreply.github.com>
2019-10-30 14:22:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,16 @@ $result = $dbConn->query('SELECT * FROM users ORDER BY id DESC');
 <html>
 <head>	
 	<title>Homepage</title>
+	<script src="https://kit.fontawesome.com/3e052f9cf3.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="alternate/style.css">
 </head>
 
 <body>
-<a class="create" href="create.php">Create new user</a><br/><br/>
+<main>
+<h1>Users</h1>
+	<table cellspacing="0">
 
-	<table width='80%' border=0>
-
-	<tr bgcolor='#CCCCCC'>
+	<tr>
 		<th>Username</th>
 		<th>Email</th>
 		<th>Password</th>
@@ -23,14 +25,20 @@ $result = $dbConn->query('SELECT * FROM users ORDER BY id DESC');
 	</tr>
 	<?php 	
 	while($row = $result->fetch(PDO::FETCH_ASSOC)) { 		
-		echo '<tr>';
+		echo '<tr class="table-rows">';
 		echo '<td>'.$row['username'].'</td>';
 		echo '<td>'.$row['email'].'</td>';
 		echo '<td>'.$row['password'].'</td>';	
-		echo "<td><a class='update' href=\"update.php?id=$row[id]\">Edit</a> | <a class='delete' href=\"delete.php?id=$row[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+		echo "<td class='td-edit' ><a class='update' href=\"update.php?id=$row[id]\"><i class='fas fa-edit'></i></a>  <a class='delete' href=\"delete.php?id=$row[id]\" onClick=\"return confirm('Are you sure you want to delete?')\"><i class='fas fa-trash'></i></a></td>";		
+		echo '</tr>';
 	}
 	?>
 	</table>
+	<br>
+	<a class="create" href="create.php">Create new user</a><br/><br/>
+</main>
 </body>
 </html>
+
+
 
