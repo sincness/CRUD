5989b387a5b62e2475fa4dce6cfdb618550175e4
Maltheboss <maltheminecraft@gmail.com>
2019-10-30 14:27:08 +0100

    Update create.php

--- create.php
+++ create.php
@@ -3,52 +3,61 @@
     <title>Create user</title>
 </head>
 
-<!-- Adding data
-validation of checking if the entered name, email & password fields are empty or not.
-If they are all filled then the data will be inserted into database table. -->
+<form action="create.php" method="post" name="form1">
+      <table width="25%">
+        <tr>
+          <td>Name</td>
+          <td><input type="text" name="name" /></td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td><input type="text" name="email" /></td>
+        </tr>
+        <tr>
+          <td>Password</td>
+          <td><input type="text" name="pass" /></td>
+        </tr>
+        <tr>
+          <td><input type="submit" name="Submit" value="Create" /></td>
+        </tr>
+        <tr>
+          <td><input type="reset" name="Submit" value="Cancel" /></td>
+        </tr>
+      </table>
+    </form>
 
 <body>
 <?php
 //including the database connection file
 include_once "alternate/db.php";
-
 if (isset($_POST['Submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $pass = $_POST['pass'];
-
     // checking empty fields
     if (empty($name) || empty($email) || empty($pass)) {
-
         if (empty($name)) {
             echo "<font color='red'>Name field is empty.</font><br/>";
         }
-
         if (empty($email)) {
             echo "<font color='red'>Email field is empty.</font><br/>";
         }
-
         if (empty($pass)) {
             echo "<font color='red'>Password field is empty.</font><br/>";
         }
-
         //link to the previous page
         echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
     } else {
         // if all the fields are filled (not empty)
-
         //insert data to database
         $sql = "INSERT INTO users(username, email, password) VALUES(:name, :email, :pass)";
         $query = $dbConn->prepare($sql);
-
         $query->bindparam(':name', $name);
         $query->bindparam(':email', $email);
         $query->bindparam(':pass', $pass);
         $query->execute();
-
         // Alternative to above bindparam and execute
         // $query->execute(array(':name' => $name, ':email' => $email, ':pass' => $pass));
-
         //display success message
         echo "<font color='green'>Data added successfully.";
         echo "<br/><a href='index.php'>View Result</a>";
